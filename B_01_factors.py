5b2c8a02e5a666b74a79a06512aafad90cc580d3
Dont-Mind-This-Account3223 <robinsonl3223@masseyhigh.school.nz>
2024-10-27 13:20:20 +1300

    working factor finder

--- B_01_factors.py
+++ B_01_factors.py
@@ -41,6 +41,7 @@ def num_check(question):
         except ValueError:
             print(error)
 
+
 def factor(var_to_factor):
     factors_list = []
     # to_factor = num_check("To factor: ")
@@ -79,9 +80,45 @@ if want_instructions == "":
     instructions()
 
 while True:
-    to_factor = num_check("To factor: ")
-    print("You chose to factor", to_factor)
+
+    comment = ""
+
+    # ask user for number to be factorised
+    to_factor = num_check("\nEnter an integer (or xxx to quit): ")
 
     if to_factor == "xxx":
         break
 
+    # get factors for integers that are 2 or more
+    elif to_factor != 1:
+        all_factors = factor(to_factor)
+
+    # set up comment for unity
+    else:
+        all_factors = ""
+        comment = "One is UNITY! It only has one factor. Itself :)"
+
+    # comments for squares / primes
+
+    # prime numbers have only two factors
+    if len(all_factors) == 2:
+        comment = f"{to_factor} is a prime number"
+
+    # check if the list has an odd number of factors
+    elif len(all_factors) % 2 == 1:
+        comment = f"{to_factor} is a perfect square"
+
+    # set up headings
+    if to_factor > 1:
+        heading = f"Factors of {to_factor}"
+    else:
+        heading = "One is special..."
+
+    # output factors and comment
+    print()
+    statement_generator(heading, "*")
+    print(all_factors)
+    print(comment)
+    print("Thank you for using the factors calculator")
+
+print("Thank you for using the factors calculator")
